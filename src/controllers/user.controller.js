a75f93ffc20adc0ceed2bd3b1c411231e04552cf
khun <lykhun@gmail.com>
2024-01-14 20:31:34 +0100

    connected  state

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,19 @@
 import { UserDTO } from '../dto/user.dto.js';
-import { User } from '../models/user.model.js'
+import { Message } from '../models/message.model.js';
 
 export const UserController = {
   getKnownUsers: async (req, res) => {
-    const users = await User.find();
+    const messages = await Message.find({
+      $or: [{ from: req.user.id }, { to: req.user.id }]
+    }).populate('from').populate('to');
+    const users = messages
+      .map(m => m.from.id == req.user.id ? m.to : m.from)
+      .reduce((prev, next) => {
+        if(!prev.some(u => u.id === next.id)) {
+          prev = [...prev, next];
+        }
+        return prev;
+      }, []);
     res.json(users.map(u => new UserDTO(u)));
   }
 }
